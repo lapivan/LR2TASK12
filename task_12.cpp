e3856db1c0bd7dd24fc7418b970dfa6e3501990b
ivan <lapunoujohn@gmail.com>
2024-09-22 16:35:43 +0300

    mysqrt

--- task_12.cpp
+++ task_12.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
-#include <cmath>
+
+double mysqrt(double x) {
+    int terms = 20;
+    if (x < 0) {
+        std::cerr << "Ошибка: нельзя вычислить квадратный корень отрицательного числа." << std::endl;
+        return -1;
+    }
+
+    // Начальное приближение
+    double guess = x / 2.0; // Начинаем с половины числа
+    double previous_guess;
+
+    for (int i = 0; i < terms; ++i) {
+        previous_guess = guess;
+        guess = (guess + x / guess) / 2.0; // Метод Ньютона
+        // Проверяем, достигли ли мы достаточной точности
+        if (guess == previous_guess)
+            break;
+    }
+
+    return guess;
+}
 int main() {
 std::cout << "ax^4 + bx^3 + cx^2 + bx + a = 0" << std::endl;
 //a(x^2+1/x^2) + b(x + 1/x) +c =0
@@ -13,16 +34,16 @@ std::cin >> c;
 //замена t = x + 1/x
 d = (b * b) - (4 * a * (c - (2 * a)));
 if (d > 0) {
-    t1 = (-b - sqrt(d)) / (2 * a);
-    t2 = (-b + sqrt(d)) / (2 * a);
+    t1 = (-b - mysqrt(d)) / (2 * a);
+    t2 = (-b + mysqrt(d)) / (2 * a);
     if (t1 >= 2 || t1 <= -2) {              //одз
-    x1 = ((t1 - sqrt(((t1 * t1) - 4))) / 2);
-    x2 = ((t1 + sqrt(((t1 * t1) - 4))) / 2);
+    x1 = ((t1 - mysqrt(((t1 * t1) - 4))) / 2);
+    x2 = ((t1 + mysqrt(((t1 * t1) - 4))) / 2);
     std::cout << x1 << x2; 
     }   
     if (t2 >= 2 || t2 <= -2) {
-    x3 = ((t2 - sqrt((t2 - 2) * (t2 + 2))) / 2);
-    x4 = ((t2 + sqrt(((t2 * t2) - 4))) / 2);
+    x3 = ((t2 - mysqrt((t2 - 2) * (t2 + 2))) / 2);
+    x4 = ((t2 + mysqrt(((t2 * t2) - 4))) / 2);
     std::cout << x3 << ' ' << x4; 
     }
     if ((t1 < 2 && t1 > -2) && (t2 < 2 && t2 > -2))
@@ -32,8 +53,8 @@ if (d > 0) {
 else if (d = 0) {
     t1 = (-b / (2 * a));
     if (t1 >= 2 || t1 <= -2) {
-    x1 = ((t1 - sqrt(((t1 * t1) - 4)) ) / 2);
-    x2 = ((t1 + sqrt(((t1 * t1) - 4))) / 2);
+    x1 = ((t1 - mysqrt(((t1 * t1) - 4)) ) / 2);
+    x2 = ((t1 + mysqrt(((t1 * t1) - 4))) / 2);
     std::cout << x1 << x2; 
     } 
     else 
